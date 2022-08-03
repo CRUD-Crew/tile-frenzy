67e77e077a89f5a8cdf9f91b0c25df4008676cf2
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-08-02 20:55:23 -0500

    demonstration branch PR

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const dbConnectionStr = process.env.DB_STRING;
 const MongoClient = require('mongodb').MongoClient;
 
+
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
-        console.log(`Connected to Database`)
-    })
+        console.log(`Connected to Database`);
+    });
 
 
 app.listen(PORT, () => {
-    console.log(`listening on PORT ${PORT}`)
+    console.log(`listening on PORT ${PORT}`);
 });
