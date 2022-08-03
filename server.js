98620b332b98321dfa2474c20366373bb4ab84ea
Pree <siktheday@gmail.com>
2022-08-02 21:45:15 -0400

    added DB_STRING and connected to MongoClient

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 
 require('dotenv').config()
 const PORT = process.env.PORT || 3000;
-
+const dbConnectionStr = process.env.DB_STRING;
 const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to Database`)
+    })
+
 
 app.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`)
